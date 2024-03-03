d320058202ce62dc6ecdaf321b126d8b5dfe89e1
ElaineTavares <etsets.tavares@gmail.com>
2024-03-03 09:53:39 -0300

    data anime

--- js/script.js
+++ js/script.js
@@ -91,3 +91,25 @@ const myObserver = new IntersectionObserver ((entries) =>{
 const elements = document.querySelectorAll('.hidden')
 
 elements.forEach((element) => myObserver.observe(element))
+
+//DATA ANIME PARA MAIS ELEMENTOS//
+const item = document.querySelectorAll('[data-anime');
+const animeScroll = () => {
+    const windowTop = window.pageYOffset + window.innerHeight * 0.85;
+    // console.log(windowTop)
+
+    item.forEach(element => {
+        if(windowTop > element.offsetTop) {
+            element.classList.add('animate')
+        } else{
+            element.classList.remove('animate')
+        }
+    })
+}
+
+animeScroll()
+
+window.addEventListener('scroll', () =>{
+    animeScroll()
+})
+//FIM DATA ANIME PARA MAIS ELEMENTOS//
